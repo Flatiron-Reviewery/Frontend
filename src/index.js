524ec700b9476da861046ad32a849de6bc6afc35
Billy <billybaneydev@gmail.com>
2021-08-22 16:26:52 -0500

    merging

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-function init() {
-    console.log('Initializiation')
-};
-init();
+fetch('https://api.openbrewerydb.org/breweries')
+.then(res => res.json())
+.then(brewery => renderCard(brewery))
+
+const renderCard = (brewery) => {
+    console.log(brewery);
+    const cardContainer = document.getElementById('card-container')
+    brewery.forEach(cards => {   
+        const card = document.createElement('div')
+        const title = document.createElement('h1')
+        const location = document.createElement('h1')
+        title.setAttribute('id', 'title')
+        card.setAttribute('id', 'card')
+        location.setAttribute('id', 'location')
+
+        title.innerText = cards.name
+        location.innerText = cards.city
+        card.appendChild(title)
+        card.appendChild(location)
+        cardContainer.appendChild(card)
+    })
+}
