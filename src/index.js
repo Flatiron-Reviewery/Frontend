ff80b65a29890393fa9db38cb732b43f5ec9e4e2
Peter Bacenet <peterbacenet@icloud.com>
2021-08-22 11:42:25 -0400

    Successful Card Rendering

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-fetch ("https://raw.githubusercontent.com/openbrewerydb/openbrewerydb/master/breweries.json")
-.then(resp => resp.json())
-.then(data => data.forEach(brewery => renderCard(brewery)));//returns array of OBJECTS, coverts to obj..
+fetch('https://api.openbrewerydb.org/breweries')
+.then(res => res.json())
+.then(brewery => renderCard(brewery))
 
-function renderCard(breweries) { //console.log(breweries) returns all objects
-    // const title = document.querySelector("#title")
-    const container = document.querySelector("#card")
-    const newElement = document.createElement('p')
-    newElement.textContent = breweries.name
-    container.appendChild(newElement)
+const renderCard = (brewery) => {
+    console.log(brewery);
+    const cardContainer = document.getElementById('card-container')
+    brewery.forEach(cards => {   
+        const card = document.createElement('div')
+        const title = document.createElement('h1')
+        const location = document.createElement('h1')
+        title.setAttribute('id', 'title')
+        card.setAttribute('id', 'card')
+        location.setAttribute('id', 'location')
+
+        title.innerText = cards.name
+        location.innerText = cards.city
+        card.appendChild(title)
+        card.appendChild(location)
+        cardContainer.appendChild(card)
+    })
+}
+
+
+// fetch ("https://raw.githubusercontent.com/openbrewerydb/openbrewerydb/master/breweries.json")
+// .then(resp => resp.json())
+// .then(data => data.forEach(brewery => renderCard(brewery)));//returns array of OBJECTS, coverts to obj..
+
+// function renderCard(breweries) { //console.log(breweries) returns all objects
+//     // const title = document.querySelector("#title")
+//     const container = document.querySelector("#card")
+//     const newElement = document.createElement('p')
+//     newElement.textContent = breweries.name
+//     container.appendChild(newElement)
     
 
     // const header = document.querySelector(".top")
